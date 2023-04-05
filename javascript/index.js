64cd49db084ad0e6d1bfbb2d6da7085d1601f6ea
Gasheezy <mungai.gachango@gmail.com>
2023-04-05 18:29:34 +0300

    Still

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,19 @@
-console.log('Easy Learning');
+//variables
+
+let name = 'Mungai';
+console.log(typeof name);
+
+let age = 45;
+console.log(typeof age);
+
+let isMarried = false;
+console.log(typeof isMarried);
+
+let colors = undefined;
+console.log(colors);
+
+let selectColor = null;
+console.log(typeof selectColor);
+
+
+
