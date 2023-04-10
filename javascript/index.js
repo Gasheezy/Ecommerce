e7e073cc3e4a202710fc2337a688f53ebf01dd0c
Gasheezy <mungai.gachango@gmail.com>
2023-04-10 22:40:27 +0300

    Exercise1

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,43 @@
-//------Functions----
+//------Exercise----
+let person = {
+    name : 'Mungai',
+    age : 28,
+    isMarried : true,
+    
+    homeAddress:{
+        long : 44.55,
+        lat : 33.44,
+    },
+    friends : ['Prime', 'Zara', 'Nimo'],
+};
+console.log(person);
+console.log(person.name);
+console.log(person.homeAddress.lat);
 
 
 
 /*
+//------Template Literals----
+let name = 'Mungai';
+console.log('My name is ' +name);
+console.log('${30+20}'); //not working
+
+//------Functions----
+function showMyName(name){
+    //console.log(name);
+    console.log('My name is ' +name);
+}
+showMyName('Mungai');
+showMyName('Prime');
+showMyName('Zara');
+
+function calcSum(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+const result =  calcSum(30,20);
+console.log(result);
+
 //------JavaScript Array----
 let friends = ['Jeff', 'Kim', 'Sam', 'Vee']
 console.log(friends);
